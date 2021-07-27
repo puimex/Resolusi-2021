7889030634c184f0aaa31bb541dbc86e819a535a
puimex <87854157+puimex@users.noreply.github.com>
2021-07-27 16:30:37 +0700

    update

---
+++ basic_dart/example/basic_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:basic_dart/basic_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ basic_dart/lib/basic_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library basic_dart;
+
+export 'src/basic_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
